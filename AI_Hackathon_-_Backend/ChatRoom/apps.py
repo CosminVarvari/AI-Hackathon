a905e9d94ed1dd209eeccbc2f23581f2f002fcc1
CosminVarvari <92040198+CosminVarvari@users.noreply.github.com>
2024-10-18 15:56:59 +0300

    Delete AI_Hackathon_-_Backend directory

--- AI_Hackathon_-_Backend/ChatRoom/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class ChatRoomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ChatRoom'
-
-    # Eliminăm metoda ready() care accesează baza de date prea devreme
